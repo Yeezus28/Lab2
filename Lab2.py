89fad4c606f898e02fc22e73786c97a860cc219d
Wei Guo <weiguo.22@ichat.sp.edu.sg>
2024-10-22 15:56:26 +0800

    New Update

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,24 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    number = input()
+    number_split = number.split(",")
+    number_floats = [float(num) for num in number_split]
+    return number_floats
+
+def calc_average_temperature():
+    print("calc_average")
+    return 0.02
+
+def calc_min_max_temperature():
+    print("find_min_max")
+    return [0.0, 0.0]
+
+def sort_temperature():
+    print("sort_temperature")
+    return []
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+    return 0.0
